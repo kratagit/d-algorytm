ad5f9f5aea77248ecd6bcb63f4178b47de760cbf
KrataPremium <pakratek@gmail.com>
2026-06-14 23:28:58 +0200

    Enhance error handling in D-Algorithm engine: add delta information to step messages and improve context menu interaction in index.html

--- engine.py
+++ engine.py
@@ -179,7 +179,7 @@ class DAlgorithmEngine:
                     valid_sens.append({'val': v, 'sym': 'D' if (h_out=='1' and f_out=='0') else '~D'})
                     
             if not valid_sens:
-                self.add_step(f"SPRZECZNOŚĆ (Blokada propagacji) na {n_gate['id']}! Drugie wejście ma stan '{o_state}', który uniemożliwia przepuszczenie błędu.", full=False)
+                self.add_step(f"SPRZECZNOŚĆ (Blokada propagacji) na {n_gate['id']}! Drugie wejście ma stan '{o_state}', który uniemożliwia przepuszczenie błędu.", delta={n_gate['id']: 'ERR'}, full=False)
                 conflict = True
                 break
                 
@@ -243,7 +243,7 @@ class DAlgorithmEngine:
                         s0_val = self.algo_state.get(n0_name, 'x')
                         s1_val = self.algo_state.get(n1_name, 'x')
                         msg = f"SPRZECZNOŚĆ na {cid}={val}! Bramka {g['type']} ma na wejściach: {n0_name}={s0_val}, {n1_name}={s1_val}. Funkcja logiczna bramki wyklucza taki wynik."
-                        self.add_step(msg, full=False)
+                        self.add_step(msg, delta={cid: 'ERR'}, full=False)
                         break
                         
                     opts = [{'label': ", ".join(f"{self.resolve_name(k)}={v}" for k,v in cb.items() if v!='x') or "Gotowe", 'data': cb} for cb in valid_cands]
